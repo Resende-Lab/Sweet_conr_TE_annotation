2ab33b1d593965131ed05f860a8691c61597c86d
huying2020 <68702011+huying2020@users.noreply.github.com>
2020-09-30 09:25:56 -0400

    Update SINEfinder.sh

--- SINE/SINEfinder.sh
+++ SINE/SINEfinder.sh
@@ -1,2 +1,11 @@
+# All 9 maize genomes
+Genome_list=/1_Data/9genomes_list_HelitronScanner.txt
+Genome_FASTA=$(head -n $SLURM_ARRAY_TASK_ID $Genome_list | tail -n 1)
+Genome_ID=$(echo $Genome_FASTA | cut -f 7 -d '/')
+
+        ##########################
+        # SINEfinder Transposons #
+        ##########################
+
 # Run SINEfinder (-f both : outputs csv and fasta)
-python sine_finder.py -T chunkwise -V1 -f both Genome.fasta
+python sine_finder.py -T chunkwise -V1 -f both ${Genome_FASTA}
